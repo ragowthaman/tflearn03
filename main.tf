f7f2219aa62f5a4376988935628230d0dff7942d
ragowthaman <ragowthaman@gmail.com>
2021-04-19 15:03:09 +0530

    initial commit terralithic

---
+++ main.tf
@@ -0,0 +1,123 @@
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+data "aws_ami" "latest_amazon2_ami" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = [var.os_image_name]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# security
+resource "aws_security_group" "tflearn_sg" {
+  name        = "tflearn_sg"
+  description = "TF Learn 03 security group"
+  vpc_id      = resource.aws_vpc.tf_vpc01.id
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "web port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "web port"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "tflearn_sg"
+    "project" = "tf-learn03"
+  }
+}
+
+# network
+resource "aws_vpc" "tf_vpc01" {
+  cidr_block = var.vpc_cidr_block
+  instance_tenancy = "default"
+
+  tags = {
+    Name: "tf-vpc01"
+    "project": "tf-learn03"
+  }
+  
+}
+
+resource "aws_subnet" "tflearn_subnet01_pub" {
+  vpc_id = resource.aws_vpc.tf_vpc01.id
+  cidr_block = var.subnet_cidr_block_pub
+
+  tags = {
+    Name= "${var.env_prefix}-tflearn_subnet01-pub",
+    "project" = "tf-learn03"
+  }
+  
+}
+
+
+resource "aws_subnet" "tflearn_subnet02_pvt" {
+  vpc_id = resource.aws_vpc.tf_vpc01.id
+  cidr_block = var.subnet_cidr_block_pvt
+
+  tags = {
+    Name = "${var.env_prefix}-tflearn-subnet02-pvt"
+    "project" = "tf-learn03"
+  }
+}
+
+
+resource "aws_instance" "webserver01" {
+  ami = data.aws_ami.latest_amazon2_ami
+  instance_type = var.instance_type
+  subnet_id = resource.aws_subnet.tflearn_subnet01_pub.id
+  vpc_security_group_ids = [ "resource.aws_security_group.tflearn_sg.id" ]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = false
+  key_name = "value"
+
+  tags = {
+    "project" = "tf-learn03"
+    Name = "${var.env_prefix}-Webserver instances"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {type = string}
+variable "os_image_name" {type = string}
+variable "vpc_cidr_bloc" {type = string}
+variable "subnet_cidr_block_pub" { type = string}
+variable "subnet_cidr_block_pvt" { type = string}
+variable "avail_zone" {type = string}
